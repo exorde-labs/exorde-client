ff26caefca15dd06c9eacb5ffbccf476dd6f3c5a
Patrick Borowy <patrick@exordelabs.com>
2023-03-02 11:21:54 +0100

    dev wip: ipfs

--- exc_exorde/__init__.py
+++ exc_exorde/__init__.py
@@ -10,9 +10,34 @@ a88aaaa    dP. .dP .d8888b. 88d888b. .d888b88 .d8888b.
           Supported composition for EXD mining.
 '''
 
-from exc_twitter import tweet_wire
+from exorde.bindings import wire
 
+from exc_twitter import tweet_wire
+from exc_ipfs import upload_to_ipfs
+
+PARAMETERS = {
+    'ipfs_path': {
+        'default': 'http://ipfs-api.exorde.network/add',
+    }
+}
+
+def twitter_to_exorde_format(data: dict) -> dict:
+    return data
+
+formated_tweet_trigger, formated_tweet_wire = wire()
 @tweet_wire
-def test(tweet):
-    print('----------')
-    print(tweet['full_text'])
+async def format_tweet(tweet):
+    await formated_tweet_trigger(twitter_to_exorde_format(tweet))
+
+async def spot(data, ipfs_path):
+    '''
+    Spotting data happens in two steps :
+        - upload_to_ipfs
+        - send a transaction to say "Job Done !"
+    '''
+    await upload_to_ipfs(data, ipfs_path)
+
+formated_tweet_wire(spot)
+
+# 1. retrieve tweets -> upload to ipfs -> data_spotting
+# 2. get_job -> check
