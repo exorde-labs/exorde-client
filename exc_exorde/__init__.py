44e1c03b79d83580a12426a836c528e84bb29fd1
Patrick Borowy <patrick@exordelabs.com>
2023-03-09 09:34:16 +0100

    dev: wip

--- exc_exorde/__init__.py
+++ exc_exorde/__init__.py
@@ -6,17 +6,20 @@ a88aaaa    dP. .dP .d8888b. 88d888b. .d888b88 .d8888b.
 88         `8bd8'  88'  `88 88'  `88 88'  `88 88ooood8 '
 88         .d88b.  88.  .88 88       88.  .88 88.  ...
 88888888P dP'  `dP `88888P' dP       `88888P8 `88888P'  S
+  
+  Supported interface implementation for EXD mining.
 
-          Supported composition for EXD mining.
 '''
+import logging
 import aiohttp
 import asyncio
 import os
+import random
 import json
 import yaml
-import random
 
 from exorde.bindings import wire
+from exorde.utils import read_only 
 
 from web3 import Web3
 from web3.middleware.cache import _simple_cache_middleware as cache_middleware
@@ -31,50 +34,74 @@ PARAMETERS = {
     }
 }
 
-def choose_w3_gateway(w3_instances):
-    random.choice(w3_instances)
+def load_yaml(path):
+    with open(path, 'r') as _file:
+        yaml_data = yaml.safe_load(_file)
+        return yaml_data
 
-async def instanciate_w3():
-    def load_yaml(path):
-        with open(path, 'r') as _file:
-            yaml_data = yaml.safe_load(_file)
-            return yaml_data
+async def fetch(session, url):
+    async with session.get(url) as response:
+        return await response.json(content_type=None)
 
-    async def fetch(session, url):
-        async with session.get(url) as response:
-            return await response.json(content_type=None)
+configuration = lambda: {
+    'configuration': load_yaml(
+        os.path.dirname(os.path.abspath(__file__)) + '/configuration.yaml'
+    )
+}
 
-    # retrieve configuration
-    current_directory = os.path.dirname(os.path.abspath(__file__))
-    network = load_yaml(f'{current_directory}/network.yaml')
-    protocol = load_yaml(f'{current_directory}/protocol.yaml')
+# cnf stands for configuration
+# Data-coupling because contract instanciation requires Contract & ABIS 
+async def contracts_and_abi_cnf(configuration):
     async with aiohttp.ClientSession() as session:
         requests = []
-        for __name__, path in protocol['ABI'].items():
-            url = f"{protocol['source']}{path}"
+        for __name__, path in configuration['ABI'].items():
+            url = f"{configuration['source']}{path}"
             request = asyncio.create_task(fetch(session, url))
             requests.append(request)
         abis = await asyncio.gather(*requests)
         contracts = await fetch(
             session,
-            f"{protocol['source']}/{protocol['contracts']}"
+            f"{configuration['source']}/{configuration['contracts']}"
         )
-
-    # instanciate web3 instances
-    w3_instances = []
-    for _urlSkale in network[protocol['target']]['_urlSkale']:
-        w3_instance = Web3(Web3.HTTPProvider(_urlSkale))
-        w3_instance.middleware_onion.add(cache_middleware)
-        w3_instances.append({
-            'w3': w3_instance,
-        })
-
+    logging.info('abis loaded are : %s', ', '.join([abi['contractName'] for abi in abis]))
     return {
-        'network': network,
-        'protocol': protocol,
-        'w3_instances': w3_instances,
-        'abi': { abi['contractName']: abi for abi in abis }
+        'contracts_cnf': read_only(**contracts),
+        'abi_cnf': read_only(**{ abi['contractName']: abi for abi in abis })
     }
+
+def instanciate_w3(url):
+    w3_instance = Web3(Web3.HTTPProvider(url))
+    w3_instance.middleware_onion.add(cache_middleware)
+    return w3_instance
+
+write_web3 = lambda configuration: {
+    'write_web3': instanciate_w3(
+        configuration[configuration['target']]['_urlTxSkale']
+    )
+}
+
+read_web3 = lambda configuration: {
+    'read_web3': instanciate_w3(
+        random.choice(
+            configuration[configuration['target']]['_urlSkale']
+        )
+    )
+}
+
+contract = lambda name, read_w3, abi_cnf, contracts_cnf, configuration: read_w3.eth.contract(
+    contracts_cnf()[configuration['target']][name],
+    abi=abi_cnf()[name]["abi"]
+)
+
+contracts = lambda read_w3, abi_cnf, contracts_cnf, configuration: {
+    name: contract(name, read_w3, abi_cnf, contracts_cnf, configuration) 
+    for name in contracts_cnf()[configuration['target']]
+}
+
+reset_transactions = lambda: {
+    'transactions': [],
+    'nounce': None 
+}
 
 def register_main_erc_address():
     '''Transaction which binds an ethereum address to workers'''
@@ -100,27 +127,26 @@ def claim_data_batch():
     # })
     # self.app.tm.waitingRoom.put((increment_tx, self.app.localconfig["ExordeApp"]["ERCAddress"], self.app.pKey))
 
-def twitter_to_exorde_format(data: dict) -> dict:
-    return {
-        "Author": "",
-        "Content": data['full_text'],
-        "Controversial": data.get('possibly_sensitive', False),
-        "CreationDateTime": data['created_at'],
-        "Description": "",
-        "DomainName": "twitter.com",
-        "Language": data['lang'],
-        "Reference": "",
-        "Title": "",
-        "Url": f"https://twitter.com/a/status/{data['id_str']}",
-        "internal_id": data['id'],
-        "media_type": "",
-        # "source": data['source'], # new
-        # "nbQuotes": data['quote_count'], # new
-        "nbComments": data['reply_count'],
-        "nbLikes": data['favorite_count'],
-        "nbShared": data['retweet_count'],
-        # "isQuote": data['is_quote_status'] # new
-    }
+twitter_to_exorde_format = lambda data: {
+    "Author": "",
+    "Content": data['full_text'],
+    "Controversial": data.get('possibly_sensitive', False),
+    "CreationDateTime": data['created_at'],
+    "Description": "",
+    "DomainName": "twitter.com",
+    "Language": data['lang'],
+    "Reference": "",
+    "Title": "",
+    "Url": f"https://twitter.com/a/status/{data['id_str']}",
+    "internal_id": data['id'],
+    "media_type": "",
+    # "source": data['source'], # new
+    # "nbQuotes": data['quote_count'], # new
+    "nbComments": data['reply_count'],
+    "nbLikes": data['favorite_count'],
+    "nbShared": data['retweet_count'],
+    # "isQuote": data['is_quote_status'] # new
+}
 
 formated_tweet_trigger, formated_tweet_wire = wire(batch=100)
 async def format_tweet(tweet, **kwargs):
--- exc_exorde/bindings.py
+++ exc_exorde/bindings.py
@@ -1,17 +1,29 @@
-from exorde.bindings import setup, curse
+
+
+from exorde.bindings import routine, oversight
 
 from exc_twitter import bindings as __bindings__
 from exc_twitter import tweet_wire
 
-from . import (
+from exc_exorde import (
+    configuration,
+    contracts_and_abi_cnf,
+    contracts,
     format_tweet,
     spot,
     formated_tweet_wire,
-    instanciate_w3,
-    choose_w3_gateway
+    read_web3,
+    write_web3
 )
 
-setup()(instanciate_w3)
+# routines with timeout= 0 are executed instantly
+routine(60 * 5, timeout=0)(configuration)
+oversight('configuration')(contracts_and_abi_cnf)
+# choose a url_Skale and instanciate web3
+oversight('configuration')(write_web3)
+oversight('configuration')(read_web3)
+oversight('read_web3')(contracts)
+
+# tweet retrieval
 tweet_wire(format_tweet)
 formated_tweet_wire(spot)
-curse('w3_gateway')(choose_w3_gateway)
