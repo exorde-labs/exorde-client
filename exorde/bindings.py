9220b453620703801fc0619db19a934f24f0c2d7
Patrick Borowy <patrick@exordelabs.com>
2023-03-26 09:37:33 +0200

    fix(session): correct usage in regards to vaulted aiosow.http

--- exorde/bindings.py
+++ exorde/bindings.py
@@ -1,52 +1,54 @@
-'''
+"""
 Composition for EXD Mining.
 
 TODOS:
     - check for main eth address
     - cache
     - smoother request frequency
-'''
+"""
+
+from aiohttp import ClientSession
 
 from aiosow.bindings import on, wrap, wire, option, alias, accumulator, setup
 from aiosow.routines import routine, spawn_consumer
-
 from aiosow_twitter.bindings import on_tweet_reception_do
-from aiosow.http import aiohttp_session
 
 from exorde import *
 
-option('ethereum_address', help='Ethereum wallet address', default=None)
+option("ethereum_address", help="Ethereum wallet address", default=None)
 
 # setup an aiohttp session for ipfs upload
-setup(wrap(lambda session: { 'session': session })(aiohttp_session))
-setup(wrap(lambda schema: { 'ipfs_schema': schema })(load_json_schema))
-alias('ipfs_path')(lambda: 'http://ipfs-api.exorde.network/add')
+setup(wrap(lambda session: {"session": session})(lambda: ClientSession()))
+setup(wrap(lambda schema: {"ipfs_schema": schema})(load_json_schema))
+alias("ipfs_path")(lambda: "http://ipfs-api.exorde.network/add")
 
 # instanciate workers
-setup(wrap(lambda acct: {
-    'worker_address': acct.address, 'worker_key': acct.key
-})(worker_address))
+setup(
+    wrap(lambda acct: {"worker_address": acct.address, "worker_key": acct.key})(
+        worker_address
+    )
+)
 
 # retrieve configuration
 routine(60 * 5, life=0)(configuration)
 
 # retrieve contracts and abi
-on('configuration')(contracts_and_abi_cnf)
+on("configuration")(contracts_and_abi_cnf)
 
 # choose a url_Skale and instanciate web3
-on('configuration')(write_web3)
-on('configuration')(read_web3)
+on("configuration")(write_web3)
+on("configuration")(read_web3)
 
 # instanciate contracts
-on('read_web3')(contracts)
+on("read_web3")(contracts)
 
 # on transactions or nounce change try to set a new current signed_transaction
 no_signed_transaction = lambda signed_transaction: not signed_transaction
-on('transactions', condition=no_signed_transaction)(select_transaction_to_send)
-on('nounce', condition=no_signed_transaction)(select_transaction_to_send)
+on("transactions", condition=no_signed_transaction)(select_transaction_to_send)
+on("nounce", condition=no_signed_transaction)(select_transaction_to_send)
 
 # send_raw_transaction (may have to be a routine)
-on('signed_transaction', condition=lambda signed_transaction: signed_transaction)(
+on("signed_transaction", condition=lambda signed_transaction: signed_transaction)(
     send_raw_transaction
 )
 
@@ -54,7 +56,7 @@ on('signed_transaction', condition=lambda signed_transaction: signed_transaction
 # routine(1, life=5)(wrap(lambda val: {'nounce': val})(nounce))
 
 # set signed_transaction to None on nounce change
-on('nounce')(lambda: { 'signed_transaction': None })
+on("nounce")(lambda: {"signed_transaction": None})
 
 # tweet retrieval and format
 broadcast_formated, on_formated_tweet_do = wire()
