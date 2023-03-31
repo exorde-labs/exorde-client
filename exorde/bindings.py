6c11f2a2d5e38b455d3e851cb974366d02e1879a
Patrick Borowy <patrick@exordelabs.com>
2023-03-31 09:21:10 +0200

    dev(protocol): deque items for processing

--- exorde/bindings.py
+++ exorde/bindings.py
@@ -6,25 +6,36 @@ a88aaaa    dP. .dP .d8888b. 88d888b. .d888b88 .d8888b.
 88         .d88b.  88.  .88 88       88.  .88 88.  ...
 88888888P dP'  `dP `88888P' dP       `88888P8 `88888P'  S
   
-  Supported interface implementation for EXD mining.
+  Supported composition for EXD mining.
 """
 
-import exorde.playwright.bindings as _
-import exorde.protocol.bindings as _
-from exorde.translation.bindings import on_translated_do, translate
+import logging
+
 from exorde.twitter.bindings import on_formated_tweet_do
-from exorde.ipfs.bindings import push_to_ipfs, on_new_cid_do
+from exorde.translation.bindings import on_translated_do, translate
 from exorde.xyake.bindings import on_keywords_extracted_do, populate_keywords
+from exorde.ipfs.bindings import push_to_ipfs, on_new_cid_do
+import exorde.protocol.bindings as _
+from aiosow.routines import routine
 
-# when a tweet is formated, push it to translation
-on_formated_tweet_do(translate)
+from collections import deque
 
-on_translated_do(populate_keywords)
+stack = deque(maxlen=1000)
 
-on_keywords_extracted_do(lambda value: print(value))
 
-when_item_is_ready = lambda v: v
-when_item_is_ready(push_to_ipfs)
+def push_to_stack(value):
+    global stack
+    stack.append(value)
 
-# print new cids
-on_new_cid_do(lambda value: print(value))
+
+def print_stack():
+    logging.info(f"{len(stack)} items ready to processed")
+
+
+routine(1)(print_stack)
+
+on_formated_tweet_do(push_to_stack)
+# on_formated_tweet_do(translate)
+# on_translated_do(populate_keywords)
+# on_keywords_extracted_do(push_to_ipfs)
+# on_new_cid_do(lambda value: print(value))
