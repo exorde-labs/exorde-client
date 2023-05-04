9c5b717be54952da9d12d663c5fedd4addd982b3
Mathias Dail <93382891+MathiasExorde@users.noreply.github.com>
2023-05-04 15:18:12 +0200

    Update __init__.py
    added swifter, optimization

--- exorde/meta_tagger/__init__.py
+++ exorde/meta_tagger/__init__.py
@@ -206,12 +206,12 @@ def tag(documents, nlp, device, mappings):
     
     # Compute sentence embeddings
     model = SentenceTransformer('sentence-transformers/all-MiniLM-L6-v2')
-    tmp["Embeddings"] = tmp["Translation"].apply(lambda x: model.encode(x))
+    tmp["Embedding"] = tmp["Translation"].swifter.apply(lambda x: model.encode(x))
     
     # Text classification pipelines
     text_classification_models = [
         ("Advertising", "djsull/kobigbird-spam-multi-label"),
-        ("Emotion", "j-hartmann/emotion-english-distilroberta-base"),
+        ("Emotion", "SamLowe/roberta-base-go_emotions"),
         ("Irony", "cardiffnlp/twitter-roberta-base-irony"),
         ("LanguageScore", "salesken/query_wellformedness_score"),
         ("TextType", "marieke93/MiniLM-evidence-types"),
@@ -219,12 +219,12 @@ def tag(documents, nlp, device, mappings):
     ]
     for col_name, model_name in text_classification_models:
         pipe = pipeline("text-classification", model=model_name, top_k = None, device=device)
-        tmp[col_name] = tmp["Translation"].apply(lambda x: [(y["label"], y["score"]) for y in pipe(x)[0]])
+        tmp[col_name] = tmp["Translation"].swifter.apply(lambda x: [(y["label"], y["score"]) for y in pipe(x)[0]])
         del pipe # free ram for latest pipe
     
     # Tokenization for custom models
     tokenizer = AutoTokenizer.from_pretrained("bert-large-uncased")
-    tmp["Embedded"] = tmp["Translation"].apply(lambda x: np.array(tokenizer.encode_plus(x, add_special_tokens=True, max_length=512, truncation=True, pad_to_max_length=True, return_attention_mask=False, return_tensors='tf')["input_ids"][0]).reshape(1, -1))
+    tmp["Embedded"] = tmp["Translation"].swifter.apply(lambda x: np.array(tokenizer.encode_plus(x, add_special_tokens=True, max_length=512, truncation=True, pad_to_max_length=True, return_attention_mask=False, return_tensors='tf')["input_ids"][0]).reshape(1, -1))
     
     # Sentiment analysis using VADER
     emoji_lexicon = hf_hub_download(repo_id="ExordeLabs/SentimentDetection", filename="emoji_unic_lexicon.json")
@@ -236,7 +236,7 @@ def tag(documents, nlp, device, mappings):
     sentiment_analyzer = SentimentIntensityAnalyzer()
     sentiment_analyzer.lexicon.update(Loughran_dict)
     sentiment_analyzer.lexicon.update(unic_emoji_dict)
-    tmp["Sentiment"] = tmp["Translation"].apply(lambda x: sentiment_analyzer.polarity_scores(x)['compound'])
+    tmp["Sentiment"] = tmp["Translation"].swifter.apply(lambda x: sentiment_analyzer.polarity_scores(x)['compound'])
     
     # Custom model pipelines
     custom_model_data = [
@@ -248,7 +248,7 @@ def tag(documents, nlp, device, mappings):
     for col_name, repo_id, file_name in custom_model_data:
         model_file = hf_hub_download(repo_id=repo_id, filename=file_name)
         custom_model = tf.keras.models.load_model(model_file, custom_objects={"TokenAndPositionEmbedding": TokenAndPositionEmbedding, "TransformerBlock": TransformerBlock})
-        tmp[col_name] = tmp["Embedded"].apply(lambda x: predict(x, custom_model, col_name, mappings))
+        tmp[col_name] = tmp["Embedded"].swifter.apply(lambda x: predict(x, custom_model, col_name, mappings))
         del custom_model # free ram for latest custom_model
 
     # The output is a list of dictionaries, where each dictionary represents a single input text and contains
@@ -292,22 +292,12 @@ def meta_tagger_initialization():
     }
 
 
+
 if __name__ == "__main__":
     init = meta_tagger_initialization()
 
-    # text = """
-    # Kevin McCarthy has quietly implemented a pay raise for members that could be $30,000+ per person. It circumvents the Constitution by instead reimbursing their rent, utilities, & meals.
-
-    # While complaining about the debt and voting to cut veterans’ benefits.
-    # """
-    # zero_shotter(text, init["labels"], init["classifier"])
-    # field = zero_shot(test, labels, max_depth=1) ==> ['Economy and Finance', 'Lifestyle and Traditions']
-    # print(field)
-    
-    print("testing tag")
     ### TEST ZONE
-    test =  [
-        "Bitcoin hit 30.000$ last night!",
+    test = ["Bitcoin hit 30.000$ last night!",
         "I like having a mojito with my breakfast",
         "Apple Inc. reports a 5% increase in revenue this quarter",
         "Get the best deal on laptops! Save up to 50% on our online store!",
@@ -316,7 +306,108 @@ if __name__ == "__main__":
         "Microsoft acquires another startup, expanding its cloud services",
         "Try our 30-day free trial of premium membership, and enjoy unlimited access!",
         "Oil prices continue to surge amid geopolitical tensions",
-        "Eager to travel again? Book your dream vacation now with our special discounts!",
-    ]
+        "Eager to travel again? Book your dream vacation now with our special discounts!"
+        "Bitcoin hits new record high of $70,000",
+        "Amazon reports record-breaking sales during Black Friday and Cyber Monday",
+        "New exhibition of Van Gogh's paintings opens at the Louvre",
+        "Tesla stocks reach all-time high as demand for electric cars surges",
+        "World Cup 2022 tickets go on sale, with prices starting from $100",
+        "Gold prices hit a 6-month high amid rising inflation concerns",
+        "Facebook faces antitrust lawsuits from the US government and multiple states",
+        "Olympic Games 2024 to be held in Paris, France",
+        "Federal Reserve hints at potential interest rate hike in the coming months",
+        "McDonald's introduces new plant-based burger to its menu",
+        "Elon Musk surpasses Jeff Bezos to become the world's richest person",
+        "New York Stock Exchange suspends trading due to technical glitch",
+        "Walt Disney Company reports strong earnings for Q3",
+        "Rafael Nadal wins his 21st Grand Slam title at the Australian Open",
+        "China's economy grows by 8% in 2022, despite global pandemic",
+        "Starbucks pledges to become more eco-friendly by phasing out single-use cups",
+        "NFL announces new playoff format, with 14 teams qualifying",
+        "UK economy shrinks by 1.5% in Q4, amid Brexit uncertainty",
+        "Nike releases new line of sustainable sneakers, made from recycled materials",
+        "Greta Thunberg named Time's Person of the Year for 2022",
+        "FIFA World Cup 2026 to be jointly hosted by USA, Canada, and Mexico",
+        "Apple unveils its new iPhone 15, with advanced AI features",
+        "Facebook changes its name to Meta, focuses on virtual reality and the metaverse",
+        "Novak Djokovic wins a record-breaking 10th Australian Open title",
+        "Delta Airlines announces new routes and expanded services for 2023",
+        "Google faces antitrust investigation from the European Union",
+        "Real Madrid wins the UEFA Champions League for the 15th time",
+        "US unemployment rate drops to 3.8%, lowest in 50 years",
+        "Sony releases highly anticipated PS6 console, with 8K graphics",
+        "McKinsey & Company accused of advising opioid makers on how to 'turbocharge' sales",
+        "Tokyo Olympics officially begin, after being postponed due to COVID-19",
+        "Uber and Lyft announce plans to go public in 2023",
+        "Artificial intelligence surpasses human performance in complex games like Go and chess",
+        "UK announces plans to ban petrol and diesel cars by 2030",
+        "Netflix shares drop by 10% after Q4 earnings report misses expectations",
+        "NASA launches new Mars rover, with hopes of finding signs of ancient life",
+        "Jeff Bezos steps down as CEO of Amazon, to focus on space venture Blue Origin",
+        "Floyd Mayweather Jr. announces his retirement from boxing",
+        "Bitcoin crashes by 30% in a single day, wiping out billions in value",
+        "Ford unveils its new electric F-150 Lightning pickup truck",
+        "Twitter bans former US president Donald Trump, citing incitement to violence",
+        "Serena Williams wins her 24th Grand Slam title at Wimbledon",
+        "Microsoft announces major expansion of its data center network",
+        "US government announces new infrastructure bill, with $1 trillion in spending",
+        "LeBron James signs historic $200 million contract with the Los Angeles Lakers",
+        "Wow, I'm so glad Bitcoin hit 30k, I was just about to use it to buy groceries.",
+        "Having a mojito with breakfast? Yeah, that's definitely a great way to start your day on the right foot... or stumble.",
+        "Apple's 5% increase in revenue is really going to help all those struggling millionaires out there.",
+        "50% off laptops? Because who needs to pay full price for a device that will become obsolete in a year?",
+        "Yay, the Federal Reserve is raising interest rates! Just what everyone needs, more debt to pay off.",
+        "Oh good, because what we all need is more skincare products to clutter our bathroom shelves.",
+        "Another startup for Microsoft to swallow up? How exciting, I can hardly wait for the monopolization of the entire tech industry.",
+        "Unlimited access to what, exactly? More ads and spam?",
+        "Oil prices are surging? Time to invest in bicycles and public transportation, folks.",
+        "Special discounts on travel? Just what we all need during a global pandemic!",
+        "70k for a Bitcoin? Oh boy, I can't wait to sell my kidneys for some digital currency.",
+        "Record-breaking sales during Black Friday and Cyber Monday? Because nothing screams holiday spirit like capitalist excess and greed.",
+        "A new exhibition of Van Gogh's paintings at the Louvre? Perfect, let's all go take selfies with them and completely miss the point of his art.",
+        "Tesla stocks at an all-time high? I'm sure this is exactly what our planet needs right now.",
+        "World Cup 2022 tickets starting at $100? Because nothing says fair competition like pricing out fans from lower income brackets.",
+        "Gold prices hitting a 6-month high? I can't wait to trade in all my possessions for a shiny rock.",
+        "Facebook facing antitrust lawsuits? Finally, some justice for all those years of invasive data mining and privacy violations.",
+        "Oh good, another ad for skincare products. Because what we all need is more pressure to live up to unrealistic beauty standards.",
+        "50% off laptops? Because who doesn't love buying outdated technology at a discount?",
+        "Yay, more debt to pay off thanks to the Federal Reserve's interest rate increase. I can hardly contain my excitement.",
+        "Get ready for the future of finance with our revolutionary new cryptocurrency, available now at an exclusive discount!",
+        "Invest in our token today and watch your profits soar to the moon! Don't miss out on this once-in-a-lifetime opportunity.",
+        "Our new blockchain-based platform is changing the game for businesses everywhere. Join the revolution now and reap the rewards!",
+        "Want to make a quick buck? Our exclusive ICO is the perfect opportunity to get in on the ground floor of the next big thing.",
+        "Looking for a new investment opportunity? Our cutting-edge technology and experienced team make us the perfect choice for your portfolio."
+        ]
+
+    test = test[:10]
+    print("Nb of text samples = ",len(test))
     tags = tag(test, init["nlp"], init["device"], init["mappings"]) 
-    print(tags)
+    field = zero_shot(test, init["labels"], init["classifier"], max_depth=1)
+
+    for sample, lbl, tag_ in zip(test, field, tags):
+        print("Input = ",sample)
+        top_gender = max(tag_['Gender'], key=lambda x: x[1])[0]
+        top_age = max(tag_['Age'], key=lambda x: x[1])[0]
+        hatespeech_max = max(tag_['HateSpeech'], key=lambda x: x[1])
+        advertising = max(tag_['Advertising'], key=lambda x: x[1])
+        top_emotion = max(tag_['Emotion'], key=lambda x: x[1])
+        top_irony = max(tag_['Irony'], key=lambda x: x[1])
+        textType = max(tag_['TextType'], key=lambda x: x[1])
+        sourceType = max(tag_['SourceType'], key=lambda x: x[1])
+
+        sentiment  = tag_['Sentiment']
+        languageScore = tag_['LanguageScore']
+
+        
+        print(f"\tTopic category:  **{lbl}**")
+        print(f"\tTop Gender: {top_gender}")
+        print(f"\tTop Age Range: {top_age}")
+        print(f"\tSentiment: {sentiment}")
+        print(f"\tHateSpeech: {hatespeech_max}")
+        print(f"\tAdvertising: {advertising}")
+        print(f"\tTop emotion: {top_emotion}")
+        print(f"\tIrony: {top_irony}")
+        print(f"\tText type: {textType}")
+        print(f"\tLanguage Score: {languageScore}")
+        print(f"\tSource type: {sourceType}")
+        print()
