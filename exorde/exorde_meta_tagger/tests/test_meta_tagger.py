caa1bf06700d3dbea6331c6a4f41d8f9543e670e
Patrick Borowy <patrick.borowy@proton.me>
2023-05-15 11:59:55 +0200

    test: zreo_shot should be serializable

--- exorde/exorde_meta_tagger/tests/test_meta_tagger.py
+++ exorde/exorde_meta_tagger/tests/test_meta_tagger.py
@@ -1,6 +1,6 @@
 import pytest, json
 
-from exorde_meta_tagger import meta_tagger_initialization, tag
+from exorde_meta_tagger import meta_tagger_initialization, tag, zero_shot
 from aiosow.autofill import autofill
 from .sample import SAMPLE
 
@@ -16,4 +16,12 @@ async def test_result_of_tag_should_be_json_serializable():
     """Result requires to be serialiable to pass trough the network."""
     test_content = SAMPLE[1:]
     result = await autofill(tag, args=[test_content], memory=CONFIG)
+    json.dumps(result)
+
+
+@pytest.mark.asyncio
+async def test_result_of_zero_shot_should_be_json_serializable():
+    """Result requires to be serialiable to pass trough the network."""
+    test_content = SAMPLE[1:]
+    result = await autofill(zero_shot, args=[test_content], memory=CONFIG)
     json.dumps(result)
