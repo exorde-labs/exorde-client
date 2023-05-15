d2d8694a53b56ba67c673b102eacb7082f54eeb5
Patrick Borowy <patrick.borowy@proton.me>
2023-05-15 15:06:57 +0200

    fix: meta_tager tests applied at singular

--- exorde/exorde_meta_tagger/tests/test_meta_tagger.py
+++ exorde/exorde_meta_tagger/tests/test_meta_tagger.py
@@ -9,14 +9,16 @@ CONFIG = meta_tagger_initialization()
 
 def test_max_depth_is_set():
     assert CONFIG["max_depth"] == 2
+    print("assert max_depth ok")
 
 
 @pytest.mark.asyncio
 async def test_result_of_tag_should_be_json_serializable():
     """Result requires to be serialiable to pass trough the network."""
-    test_content = SAMPLE[1:]
+    test_content = SAMPLE[:1]
     result = await autofill(tag, args=[test_content], memory=CONFIG)
     tag_result = json.dumps(result)
+    print("got tag result")
     with open("tag-result.json", "w") as f:
         f.write(tag_result)
 
@@ -24,8 +26,10 @@ async def test_result_of_tag_should_be_json_serializable():
 @pytest.mark.asyncio
 async def test_result_of_zero_shot_should_be_json_serializable():
     """Result requires to be serialiable to pass trough the network."""
-    test_content = SAMPLE[1:]
+    test_content = SAMPLE[:1]
+    print("starting zero_shot")
     result = await autofill(zero_shot, args=[test_content], memory=CONFIG)
     zero_shot_result = json.dumps(result)
+    print("got zero_shot result")
     with open("zero_shot-result.json", "w") as f:
         f.write(zero_shot_result)
