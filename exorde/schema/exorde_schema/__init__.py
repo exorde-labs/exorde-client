868f30c79a68f0e03fcc9f867b177c367f7ff6fa
Patrick Borowy <patrick.borowy@proton.me>
2023-05-16 03:10:09 +0200

    chore: deleted dangling useless prefix in folder names

--- exorde/schema/exorde_schema/__init__.py
+++ exorde/schema/exorde_schema/__init__.py
@@ -1,4 +1,3 @@
-from enum import Enum
 import json
 
 from jschemator import Schema
--- exorde/schema/setup.py
+++ exorde/schema/setup.py
@@ -1,13 +1,13 @@
 from setuptools import find_packages, setup
 
 setup(
-    name="exorde_models",
+    name="exorde_schema",
     version="0.0.1",
-    packages=find_packages(include="exorde_models"),
+    packages=find_packages(include="exorde_schema"),
     install_requires=["jschemator", "aiosow"],
     entry_points={
         "console_scripts": [
-            "exorde_models = exorde_models.__init__:print_schema"
+            "exorde_schema = exorde_schema.__init__:print_schema"
         ]
     },
 )
