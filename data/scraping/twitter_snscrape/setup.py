d1bdff0c2d1b35079f49ef7551f861c236663087
Mathias Dail <93382891+MathiasExorde@users.noreply.github.com>
2023-05-22 13:02:02 +0200

    Update setup.py

--- data/scraping/twitter_snscrape/setup.py
+++ data/scraping/twitter_snscrape/setup.py
@@ -4,6 +4,6 @@ setup(
     name="a7df32de3a60dfdb7a0b",
     version="0.0.1",
     packages=find_packages(),
-    install_requires=["hashlib", "exorde_data", "aiohttp", "snscrape", "regex"],
+    install_requires=["exorde_data", "aiohttp", "snscrape"],
     extras_require={"dev": ["pytest", "pytest-cov", "pytest-asyncio"]},
 )
