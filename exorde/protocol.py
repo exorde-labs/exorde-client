684a5a7d9c6800855b01d9f755bcef7b3e91c84c
Patrick Borowy <patrick.borowy@proton.me>
2023-06-09 00:05:57 +0200

    protocol.register_worker implementation

--- exorde/protocol.py
+++ exorde/protocol.py
@@ -267,6 +267,29 @@ async def faucet(
     )
 
 
+async def register(worker_account, contracts, write_web3, read_web3):
+    nonce = await read_web3.eth.get_transaction_count(worker_account.address)
+
+    transaction = (
+        contracts["DataSpotting"]
+        .functions.RegisterWorker()
+        .build_transaction(
+            {
+                "nonce": nonce,
+                "from": worker_account.address,
+            }
+        )
+    )
+    signed_transaction = read_web3.eth.account.sign_transaction(
+        transaction, worker_account.key.hex()
+    )
+    transaction_hash = await write_web3.eth.send_raw_transaction(
+        signed_transaction.rawTransaction
+    )
+
+    return transaction_hash, nonce
+
+
 async def spot_data(
     cid,
     worker_account: Account,
