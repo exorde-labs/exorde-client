5dc0d82a4ab87f0b9f6dca028488cb5296ab14b8
Mathias Dail <93382891+MathiasExorde@users.noreply.github.com>
2023-06-09 11:15:38 +0200

    Update protocol.py
    added claimMaster

--- exorde/protocol.py
+++ exorde/protocol.py
@@ -290,6 +290,28 @@ async def register(worker_account, contracts, write_web3, read_web3):
     return transaction_hash, nonce
 
 
+async def claimMaster(main_address_to_claim, worker_account, contracts, write_web3, read_web3):
+    nonce = await read_web3.eth.get_transaction_count(worker_account.address)
+
+    transaction = (
+        contracts["AddressManager"]
+        .functions.ClaimMaster(main_address_to_claim)
+        .build_transaction(
+            {
+                "nonce": nonce,
+                "from": worker_account.address,
+            }
+        )
+    )
+    signed_transaction = read_web3.eth.account.sign_transaction(
+        transaction, worker_account.key.hex()
+    )
+    transaction_hash = await write_web3.eth.send_raw_transaction(
+        signed_transaction.rawTransaction
+    )
+    return transaction_hash, nonce
+
+
 async def spot_data(
     cid,
     worker_account: Account,
