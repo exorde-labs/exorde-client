fa1732c7fe4488a99b17a1bc1b91c59b56b154e7
Mathias Dail <93382891+MathiasExorde@users.noreply.github.com>
2023-06-12 16:51:22 +0200

    Update setup.py

--- data/scraping/4chan/setup.py
+++ data/scraping/4chan/setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="ch4875eda56be56000ac",
-    version="0.0.2",
+    version="0.0.1",
     packages=find_packages(),
     install_requires=["lxml", "exorde_data", "aiohttp", "flashtext", "HTMLParser"],
     extras_require={"dev": ["pytest", "pytest-cov", "pytest-asyncio"]},
