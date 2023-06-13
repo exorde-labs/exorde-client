5df58705282b6dac86ae8ca97a4e76a5adbd7d1b
Mathias Dail <93382891+MathiasExorde@users.noreply.github.com>
2023-06-13 18:15:26 +0200

    Update pre_install.py
    simplified pre_install.py

--- exorde/pre_install.py
+++ exorde/pre_install.py
@@ -4,34 +4,33 @@ from argostranslate import package
 from typing import cast
 
 
-def pre_install():
-    models = [
-        "SamLowe/roberta-base-go_emotions",
-        "cardiffnlp/twitter-roberta-base-irony",
-        "salesken/query_wellformedness_score",
-        "marieke93/MiniLM-evidence-types",
-        "alimazhar-110/website_classification",
-        # "ExordeLabs/SentimentDetection",
-    ]
+models = [
+    "SamLowe/roberta-base-go_emotions",
+    "cardiffnlp/twitter-roberta-base-irony",
+    "salesken/query_wellformedness_score",
+    "marieke93/MiniLM-evidence-types",
+    "alimazhar-110/website_classification",
+    # "ExordeLabs/SentimentDetection",
+]
 
-    def install_hugging_face_models(models):
-        for model in models:
-            __tokenizer__ = AutoTokenizer.from_pretrained(model)
-            model = AutoModel.from_pretrained(model)
+def install_hugging_face_models(models):
+    for model in models:
+        __tokenizer__ = AutoTokenizer.from_pretrained(model)
+        model = AutoModel.from_pretrained(model)
 
-    install_hugging_face_models(models)
+install_hugging_face_models(models)
 
-    package.update_package_index()
-    available_packages = package.get_available_packages()
-    length = len(available_packages)
-    i = 0
-    for pkg in available_packages:
-        i += 1
-        print(
-            f" - installing translation module ({i}/{length}) : ({str(pkg)})"
-        )
+package.update_package_index()
+available_packages = package.get_available_packages()
+length = len(available_packages)
+i = 0
+for pkg in available_packages:
+    i += 1
+    print(
+        f" - installing translation module ({i}/{length}) : ({str(pkg)})"
+    )
 
-        # cast used until this is merged https://github.com/argosopentech/argos-translate/pull/329
-        package.install_from_path(
-            cast(package.AvailablePackage, pkg).download()
-        )
+    # cast used until this is merged https://github.com/argosopentech/argos-translate/pull/329
+    package.install_from_path(
+        cast(package.AvailablePackage, pkg).download()
+    )
