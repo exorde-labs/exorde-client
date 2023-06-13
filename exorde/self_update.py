384497cd3ece6ed7b0f42db4da61773577dfe9be
Mathias Dail <93382891+MathiasExorde@users.noreply.github.com>
2023-06-13 19:56:49 +0200

    Update self_update.py
    added try catch on self update

--- exorde/self_update.py
+++ exorde/self_update.py
@@ -5,11 +5,15 @@ from exorde.get_latest_tag import get_latest_tag
 
 
 async def self_update():
-    latest_tag = await get_latest_tag()
-    local_version = metadata.version("exorde")
-    if latest_tag != local_version:
-        exorde_repository_path = "git+https://github.com/exorde-labs/exorde-client.git#subdirectory=exorde&egg=exorde"
-        subprocess.check_call(["pip", "install", exorde_repository_path])
-        data_repository_path = "git+https://github.com/exorde-labs/exorde-client.git#subdirectory=data&egg=exorde-data"
-        subprocess.check_call(["pip", "install", data_repository_path])
-        os._exit(42)
+    try:
+        latest_tag = await get_latest_tag()
+        local_version = metadata.version("exorde")
+        if latest_tag != local_version:
+            exorde_repository_path = "git+https://github.com/exorde-labs/exorde-client.git#subdirectory=exorde&egg=exorde"
+            subprocess.check_call(["pip", "install", exorde_repository_path])
+            data_repository_path = "git+https://github.com/exorde-labs/exorde-client.git#subdirectory=data&egg=exorde-data"
+            subprocess.check_call(["pip", "install", data_repository_path])
+            os._exit(42)
+    except Exception as e:
+        print("error during self update: ",e)
+        
