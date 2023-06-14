78db75a16fb778623f8f82dca803fa688afc6e0c
Mathias Dail <93382891+MathiasExorde@users.noreply.github.com>
2023-06-14 11:25:59 +0200

    Update extract_keywords.py
    added filters on keywords

--- exorde/extract_keywords.py
+++ exorde/extract_keywords.py
@@ -1,6 +1,25 @@
 import yake
+import re
+import string
 
 from exorde.models import Keywords, Translation
+
+def filter_strings(input_list):
+    output_list = []
+    for s in input_list:
+        # Remove leading and trailing special characters
+        s = re.sub('^[^A-Za-z0-9 ]+|[^A-Za-z0-9 ]+$', '', s)
+
+        # Count the number of special characters in the string
+        special_char_count = sum([1 for char in s if char in string.punctuation])
+
+        # Check if there's any alphabetical character in the string
+        contains_letter = any(char.isalpha() for char in s)
+
+        # If the number of special characters is less than 30% of the total characters and the string contains at least one letter, add to output list
+        if special_char_count / len(s) <= 0.3 and contains_letter:
+            output_list.append(s)
+    return output_list
 
 language = "en"
 deduplication_thresold = 0.9
@@ -36,6 +55,7 @@ def extract_keywords(translation: Translation) -> Keywords:
     content: str = translation.translation        
     kx1 = _extract_keywords1(content)
     kx2 = _extract_keywords2(content)
-    kx = list(kx1)
-    kx.extend(x for x in kx2 if x not in kx)
-    return Keywords([e[0] for e in set(kx)])
+    keywords_ = list(kx1)
+    keywords_.extend(x for x in kx2 if x not in keywords_)
+    keywords = filter_strings(keywords_)
+    return Keywords([e[0] for e in set(keywords)])
