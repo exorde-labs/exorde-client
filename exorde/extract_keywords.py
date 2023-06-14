8d025ced509c8e31b0f36e15f1726c3959f436a9
Mathias Dail <93382891+MathiasExorde@users.noreply.github.com>
2023-06-14 10:22:10 +0200

    Update extract_keywords.py
    yake Keyword extraction with 2 sets of 3-gram & 1-gram

--- exorde/extract_keywords.py
+++ exorde/extract_keywords.py
@@ -3,24 +3,39 @@ import yake
 from exorde.models import Keywords, Translation
 
 language = "en"
-max_ngram_size = 3
 deduplication_thresold = 0.9
-deduplication_algo = "seqm"
+deduplication_algo = 'seqm'
 windowSize = 1
-numOfKeywords = 20
+max_ngram_size_1 = 1
+max_ngram_size_2 = 2
+numOfKeywords_1 = 5
+numOfKeywords_2 = 3
 
-kw_extractor = yake.KeywordExtractor(
+kw_extractor1 = yake.KeywordExtractor(
     lan=language,
-    n=max_ngram_size,
+    n=max_ngram_size_1,
     dedupLim=deduplication_thresold,
     dedupFunc=deduplication_algo,
     windowsSize=windowSize,
-    top=numOfKeywords,
+    top=numOfKeywords_1,
 )
 
-_extract_keywords = lambda text: kw_extractor.extract_keywords(text)
+kw_extractor2 = yake.KeywordExtractor(
+    lan=language,
+    n=max_ngram_size_2,
+    dedupLim=deduplication_thresold,
+    dedupFunc=deduplication_algo,
+    windowsSize=windowSize,
+    top=numOfKeywords_2,
+)
 
+_extract_keywords1 = lambda text: kw_extractor1.extract_keywords(text)
+_extract_keywords2 = lambda text: kw_extractor2.extract_keywords(text)
 
 def extract_keywords(translation: Translation) -> Keywords:
-    content: str = translation.translation
-    return Keywords([e[0] for e in set(_extract_keywords(content))])
+    content: str = translation.translation        
+    kx1 = _extract_keywords1(content)
+    kx2 = _extract_keywords2(content)
+    kx = list(kx1)
+    kx.extend(x for x in kx2 if x not in kx)
+    return Keywords([e[0] for e in set(kx)])
