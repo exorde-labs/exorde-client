d5546284400ec1125bd40b80452889a9b27fcae4
Mathias Dail <93382891+MathiasExorde@users.noreply.github.com>
2023-06-14 12:45:44 +0200

    Update pre_install.py
    prevent too many packages install

--- exorde/pre_install.py
+++ exorde/pre_install.py
@@ -2,7 +2,7 @@ from transformers import AutoModel, AutoTokenizer
 
 from argostranslate import package
 from typing import cast
-
+import logging
 
 models = [
     "SamLowe/roberta-base-go_emotions",
@@ -20,17 +20,34 @@ def install_hugging_face_models(models):
 
 install_hugging_face_models(models)
 
+### install (pre install) models target for English, and exclude low frequency ones to not overload the isntall
+def is_english_target(s):
+    return '→ English' in s
+
+langs_to_exclude_from_preinstall = ["Azerbaijani", "Catalan", "Esperanto", "Persian"]
+
+def is_to_exclude(s):
+    for lang in langs_to_exclude_from_preinstall:
+        if lang in s:
+            return True
+    return False
+
 package.update_package_index()
 available_packages = package.get_available_packages()
 length = len(available_packages)
 i = 0
+installed_packages = 0
 for pkg in available_packages:
     i += 1
-    print(
-        f" - installing translation module ({i}/{length}) : ({str(pkg)})"
-    )
+    
+    if( is_english_target(str(pkg)) and not is_to_exclude(str(pkg)) ):
+        print(
+            f" - installing translation module ({i}/{length}) : ({str(pkg)})"
+        )
 
-    # cast used until this is merged https://github.com/argosopentech/argos-translate/pull/329
-    package.install_from_path(
-        cast(package.AvailablePackage, pkg).download()
-    )
+        # cast used until this is merged https://github.com/argosopentech/argos-translate/pull/329
+        package.install_from_path(
+            cast(package.AvailablePackage, pkg).download()
+        )
+        installed_packages += 1
+logging.info(f"Installed Argos Lang packages: {str(installed_packages)}")
