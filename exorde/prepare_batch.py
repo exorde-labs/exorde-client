5f5a07569a21dc40d056a82fdeb4664fae7755e0
Mathias Dail <93382891+MathiasExorde@users.noreply.github.com>
2023-06-14 11:33:10 +0200

    Update prepare_batch.py
    added execution time for zero shot and max_depth parameter passing

--- exorde/prepare_batch.py
+++ exorde/prepare_batch.py
@@ -1,22 +1,27 @@
 import logging
+import time
 from exorde.item import get_item
 from exorde.models import Processed, LiveConfiguration
 from exorde.process import process
-
 
 async def prepare_batch(
     static_configuration, live_configuration: LiveConfiguration
 ) -> list[Processed]:
     batch_size: int = live_configuration["batch_size"]
+    max_depth_classification: int = live_configuration["max_depth"]
     batch: list[Processed] = []
     generator = get_item()
     lab_configuration = static_configuration["lab_configuration"]
     async for item in generator:
         try:
-            processed_item: Processed = await process(item, lab_configuration)
+            
+            start_time = time.perf_counter()
+            processed_item: Processed = await process(item, lab_configuration, max_depth_classification)
+            end_time = time.perf_counter()
+            exec_time_s = end_time - start_time
             batch.append(processed_item)
             logging.info(
-                f" + A new item has been processed {len(batch)}/{live_configuration['batch_size']}"
+                f" + A new item has been processed {len(batch)}/{live_configuration['batch_size']} - ({exec_time_s} s)"
             )
             if len(batch) == batch_size:
                 await generator.aclose()
