b83e7ef4d80c329957af6fffdd94328411dfa4b4
Mathias Dail <93382891+MathiasExorde@users.noreply.github.com>
2023-06-15 13:45:53 +0200

    Update get_latest_tag.py
    changed exorde to exorde-client when calling the api

--- exorde/get_latest_tag.py
+++ exorde/get_latest_tag.py
@@ -4,7 +4,7 @@ import aiohttp
 async def get_latest_tag():
     async with aiohttp.ClientSession() as session:
         async with session.get(
-            "https://api.github.com/repos/exorde/exorde-client/releases/latest"
+            "https://api.github.com/repos/exorde-labs/exorde-client/releases/latest"
         ) as response:
             body = await response.json()
             return body["tag_name"]
