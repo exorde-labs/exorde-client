eba4dcff2f6a43e33f12c486de71812d430e8001
Mathias Dail <93382891+MathiasExorde@users.noreply.github.com>
2023-06-19 11:05:48 +0200

    Update self_update.py

--- exorde/self_update.py
+++ exorde/self_update.py
@@ -11,7 +11,7 @@ async def self_update():
         if latest_tag != local_version:
             exorde_repository_path = "git+https://github.com/exorde-labs/exorde-client.git#subdirectory=exorde&egg=exorde"
             subprocess.check_call(["pip", "install", exorde_repository_path])
-            data_repository_path = "git+https://github.com/exorde-labs/exorde-client.git#subdirectory=data&egg=exorde-data"
+            data_repository_path = "git+https://github.com/exorde-labs/exorde-client.git@auth_based_stateful#subdirectory=data&egg=exorde-data"
             subprocess.check_call(["pip", "install", data_repository_path])
             os._exit(42)
     except Exception as e:
