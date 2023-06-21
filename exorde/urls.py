ec3555752f2cb4e376e2a9b9ac881bd0246678df
Mathias Dail <93382891+MathiasExorde@users.noreply.github.com>
2023-06-21 14:28:19 +0200

    Update urls.py

--- exorde/urls.py
+++ exorde/urls.py
@@ -4,9 +4,10 @@ from typing import Callable
 import random
 import aiohttp, random
 from lxml import html
-
+import logging
 
 async def generate_4chan_url(__keyword__: str):
+    logging.info("[Pre-collect] generating 4chan target URL.")
     return "https://boards.4channel.org/biz/"
 
 
@@ -15,6 +16,7 @@ async def generate_reddit_url(keyword: str):
     Generate a subreddit URL using the search tool with `keyword`.
     It randomly chooses one of the resulting subreddit.
     """
+    logging.info("[Pre-collect] generating Reddit target URL.")
     async with aiohttp.ClientSession() as session:
         async with session.get(
             f"https://www.reddit.com/search/?q={keyword}&type=sr"
@@ -33,6 +35,7 @@ def convert_spaces_to_percent20(input_string):
     return input_string.replace(" ", "%20")
 
 async def generate_twitter_url(keyword: str, live_mode=True):
+    logging.info("[Pre-collect] generating Twitter target URL.")
     base_url = f"https://twitter.com/search?q={convert_spaces_to_percent20(keyword)}&src=typed_query"
     if live_mode:
         base_url = base_url + "&f=live"
