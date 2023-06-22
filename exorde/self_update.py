b4c3f80ca707c3d38ccfc41796a6403984945e1e
Mathias Dail <93382891+MathiasExorde@users.noreply.github.com>
2023-06-22 23:22:32 +0200

    Update self_update.py

--- exorde/self_update.py
+++ exorde/self_update.py
@@ -7,11 +7,12 @@ import logging
 async def self_update():
     try:
         latest_tag = await get_latest_tag()
+        logging.info(f"[CLIENT UPDATE] Updating from {local_version} to version  {latest_tag}")
         local_version = metadata.version("exorde")
         if latest_tag != local_version:
-            exorde_repository_path = "git+https://github.com/exorde-labs/exorde-client.git#subdirectory=exorde&egg=exorde"
+            exorde_repository_path = "git+https://github.com/exorde-labs/exorde-client.git@chromedriver_fix#subdirectory=exorde&egg=exorde"
             subprocess.check_call(["pip", "install", exorde_repository_path])
-            data_repository_path = "git+https://github.com/exorde-labs/exorde-client.git#subdirectory=data&egg=exorde-data"
+            data_repository_path = "git+https://github.com/exorde-labs/exorde-client.git@chromedriver_fix#subdirectory=data&egg=exorde-data"
             subprocess.check_call(["pip", "install", data_repository_path])
             os._exit(42)
     except Exception as e:
