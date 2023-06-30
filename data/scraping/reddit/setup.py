35a96ddd2c3d91af30d50a51e122396c15bab754
Mathias Dail <93382891+MathiasExorde@users.noreply.github.com>
2023-06-30 11:40:00 +0200

    Update setup.py

--- data/scraping/reddit/setup.py
+++ data/scraping/reddit/setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="ap98j3envoubi3fco1kc",
-    version="0.0.3",
+    version="0.0.4",
     packages=find_packages(),
     install_requires=["lxml", "exorde_data", "aiohttp"],
     extras_require={"dev": ["pytest", "pytest-cov", "pytest-asyncio"]},
