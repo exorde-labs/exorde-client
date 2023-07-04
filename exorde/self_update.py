e18e0266b5c8efb4f9f784cc35e2f09545713a74
Mathias Dail <93382891+MathiasExorde@users.noreply.github.com>
2023-07-04 13:17:26 +0200

    Update self_update.py
    upgraded self update procedure

--- exorde/self_update.py
+++ exorde/self_update.py
@@ -1,19 +1,29 @@
 import os, subprocess
 from importlib import metadata
+from packaging import version
 
 from exorde.get_latest_tag import get_latest_tag
 import logging
+
+def normalize_version(version_string):
+    """Normalize the version string and remove leading 'v' if it exists."""
+    return version_string[1:] if version_string.startswith('v') else version_string
 
 async def self_update():
     try:
         latest_tag = await get_latest_tag()
         local_version = metadata.version("exorde")
+        # Normalize the versions
+        latest_tag = normalize_version(latest_tag)
+        local_version = normalize_version(local_version)
+
         logging.info(f"[CLIENT UPDATE] Updating from {local_version} to version  {latest_tag}")
-        if latest_tag != local_version:
-            exorde_repository_path = "git+https://github.com/exorde-labs/exorde-client.git#subdirectory=exorde&egg=exorde"
+        
+        if version.parse(latest_tag) > version.parse(local_version):
+            exorde_repository_path = "git+https://github.com/exorde-labs/exorde-client.git"
             subprocess.check_call(["pip", "install", exorde_repository_path])
             data_repository_path = "git+https://github.com/exorde-labs/exorde-client.git#subdirectory=data&egg=exorde-data"
             subprocess.check_call(["pip", "install", data_repository_path])
             os._exit(42)
     except Exception as e:
-        logging.info("[UDPATING] Error during self update: %s",e)
+        logging.info("[UPDATING] Error during self update: %s",e)
