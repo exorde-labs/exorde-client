2912d2c14f9488f248cf5ff67f56a323a68d5bf2
Mathias Dail <93382891+MathiasExorde@users.noreply.github.com>
2023-07-05 00:38:46 +0200

    Update setup.py

--- data/scraping/twitter/setup.py
+++ data/scraping/twitter/setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="a7df32de3a60dfdb7a0b",
-    version="0.0.997",
+    version="0.0.9971",
     packages=find_packages(),
     install_requires=[
         "exorde_data",
