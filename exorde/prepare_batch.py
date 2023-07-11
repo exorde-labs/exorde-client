c928be4fb22a10fbf29ea6b91b200d8e5bff99b2
Patrick Borowy <patrick.borowy@proton.me>
2023-07-11 13:41:33 +0200

    split_item

--- exorde/prepare_batch.py
+++ exorde/prepare_batch.py
@@ -3,16 +3,36 @@ import time
 from exorde.item import get_item
 from exorde.models import Processed, LiveConfiguration
 from exorde.process import process, TooBigError
-from exorde_data import Item
-from typing import Union, AsyncGenerator
+from exorde_data import Item, Content
+from typing import AsyncGenerator
 
 
 def evaluate_token_count(__item__: Processed) -> int:
     return 1
 
 
-def split_item(item: Item) -> list[tuple[int, Item]]:
-    return [(1, item)]
+def split_string_into_chunks(string, chunk_size):
+    return [
+        string[i : i + chunk_size] for i in range(0, len(string), chunk_size)
+    ]
+
+
+def split_item(item: Item, max_token_count: int) -> list[Item]:
+    if not item.content or len(item.content) <= max_token_count:
+        return [item]
+    else:
+        return [
+            Item(
+                content=Content(chunk),
+                author=item.author,
+                created_at=item.created_at,
+                domain=item.domain,
+                url=item.url,
+            )
+            for chunk in split_string_into_chunks(
+                item.content, max_token_count
+            )
+        ]
 
 
 async def prepare_batch(
@@ -22,32 +42,35 @@ async def prepare_batch(
     batch: list[tuple[int, Processed]] = []  # id, item
     generator: AsyncGenerator[Item, None] = get_item()
     lab_configuration: dict = static_configuration["lab_configuration"]
-    i = -1  # every item in the batch is ientified using an ID
+    item_id = -1
     async for item in generator:
-        i = i + 1
+        item_id = item_id + 1
         try:
             start_time: float = time.perf_counter()
             try:
                 processed_item: Processed = await process(
                     item, lab_configuration, max_depth_classification
                 )
-                batch.append((i, processed_item))
+                batch.append((item_id, processed_item))
             except TooBigError:
-                splitted: list[tuple[int, Item]] = split_item(item)
+                splitted: list[Item] = split_item(
+                    item,
+                    static_configuration["lab_configuration"][
+                        "max_token_count"
+                    ],
+                )
                 for chunk in splitted:
                     processed_chunk: Processed = await process(
-                        chunk[1], lab_configuration, max_depth_classification
+                        chunk, lab_configuration, max_depth_classification
                     )
-                    batch.append((i, processed_chunk))
+                    batch.append((item_id, processed_chunk))
             end_time: float = time.perf_counter()
             exec_time_s: float = end_time - start_time
             logging.info(
                 f" + A new item has been processed {len(batch)}/{live_configuration['batch_size']} - ({exec_time_s} s) - Source = {str(item['domain'])}"
             )
             if (
-                sum(
-                    [evaluate_token_count(item) for (__id__, item) in batch]
-                )  # this is confused and requires oversight
+                sum([evaluate_token_count(item) for (__id__, item) in batch])
                 >= live_configuration["batch_size"]
             ):
                 await generator.aclose()
