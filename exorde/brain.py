b5cb955ca46c9753a2e4ff4ac9dc67e6e6efe49e
Patrick Borowy <patrick.borowy@proton.me>
2023-07-12 14:37:26 +0200

    flatten ponderation file ; designate by domain

--- exorde/brain.py
+++ exorde/brain.py
@@ -10,43 +10,68 @@ from types import ModuleType
 
 
 LIVE_PONDERATION = "https://raw.githubusercontent.com/exorde-labs/TestnetProtocol/main/targets/modules_configuration.json"
-DEV_PONDERATION = "https://gist.githubusercontent.com/6r17/082bb252f504f80610602b0288baffd6/raw/0ad0b1f720f1ad1e6ce28da53e1550c4f7c146cd/ponderation.json"
-PONDERATION = LIVE_PONDERATION
+DEV_PONDERATION = "https://gist.githubusercontent.com/6r17/c844775ea359ce10fcc29a72834a5541/raw/3a89b917b2cea609311aefc95f3a46a6cfc066be/gistfile1.txt"
+PONDERATION_URL = LIVE_PONDERATION
+
+from dataclasses import dataclass
+from typing import Dict, List, Union
+
+
+@dataclass
+class Ponderation:
+    enabled_modules: Dict[str, List[str]]
+    generic_modules_parameters: Dict[str, Union[int, str, bool]]
+    specific_modules_parameters: Dict[str, Dict[str, Union[int, str, bool]]]
+    weights: Dict[str, int]
+
+
+async def get_ponderation() -> Ponderation:
+    async with aiohttp.ClientSession() as session:
+        async with session.get(PONDERATION_URL) as response:
+            response.raise_for_status()
+            raw_data: str = await response.text()
+            try:
+                json_data = json.loads(raw_data)
+            except Exception as error:
+                logging.error(raw_data)
+                raise error
+            enabled_modules = json_data["enabled_modules"]
+            generic_modules_parameters = json_data[
+                "generic_modules_parameters"
+            ]
+            specific_modules_parameters = json_data[
+                "specific_modules_parameters"
+            ]
+            weights = json_data["weights"]
+            return Ponderation(
+                enabled_modules=enabled_modules,
+                generic_modules_parameters=generic_modules_parameters,
+                specific_modules_parameters=specific_modules_parameters,
+                weights=weights,
+            )
 
 
 def ponderation_geter():
     memoised = None
     last_call = datetime.datetime.now()
 
-    async def get_ponderation() -> tuple[list[str], dict, dict[str, float]]:
+    async def get_ponderation_wrapper() -> Ponderation:
         nonlocal memoised, last_call
         now = datetime.datetime.now()
         if not memoised or (now - last_call) > datetime.timedelta(minutes=1):
             last_call = datetime.datetime.now()
-            async with aiohttp.ClientSession() as session:
-                async with session.get(PONDERATION) as response:
-                    response.raise_for_status()
-                    raw_data: str = await response.text()
-                    try:
-                        json_data = json.loads(raw_data)
-                    except Exception as error:
-                        logging.error(raw_data)
-                        raise error
-                    memoised = json_data
-        return (
-            memoised["enabled_modules"],
-            memoised["parameters"],
-            memoised["weights"],
-        )
+            memoised = await get_ponderation()
+        return memoised
 
-    return get_ponderation
+    return get_ponderation_wrapper
 
 
 get_ponderation = ponderation_geter()
 
 
-def choose_value(weights) -> str:
-    print(weights)
+def choose_domain(
+    weights: dict[str, int]
+) -> str:  # this will return "twitter" "weibo" etc...
     total_weight: int = sum(weights.values())
     choice: float = random.uniform(0, total_weight)
     cumulative_weight: int = 0
@@ -58,7 +83,13 @@ def choose_value(weights) -> str:
 
     # This will only execute if the choice exceeds the cumulative weight
     # Return first item if it occurs
-    return weights.items()[0][0]
+    return next(iter(weights))
+
+
+def choose_module(ponderation: Ponderation) -> str:
+    domain = choose_domain(ponderation.weights)
+    module_name = ponderation.enabled_modules[domain][0]
+    return module_name
 
 
 async def choose_keyword() -> str:
@@ -71,11 +102,11 @@ import os
 
 
 async def think() -> tuple[ModuleType, dict]:
-    __enabled_modules__, _parameters, weights = await get_ponderation()
+    ponderation: Ponderation = await get_ponderation()
     module: Union[ModuleType, None] = None
     choosen_module: str = ""
     while not module:
-        choosen_module = choose_value(weights)
+        choosen_module = choose_module(ponderation)
         try:
             module = await get_scraping_module(choosen_module)
         except:
@@ -84,9 +115,15 @@ async def think() -> tuple[ModuleType, dict]:
             )
             os._exit(-1)
     keyword = await choose_keyword()
-    common_parameters = _parameters["common_parameters"]
-    specific_parameters = _parameters["specific_parameters"][choosen_module]
-    parameters: dict[str, dict] = {"url_parameters": {"keyword": keyword}}
-    parameters.update(common_parameters)
+    generic_modules_parameters: dict[
+        str, Union[int, str, bool, dict]
+    ] = ponderation.generic_modules_parameters
+    specific_parameters: dict[
+        str, Union[int, str, bool, dict]
+    ] = ponderation.specific_modules_parameters[choosen_module]
+    parameters: dict[str, Union[int, str, bool, dict]] = {
+        "url_parameters": {"keyword": keyword}
+    }
+    parameters.update(generic_modules_parameters)
     parameters.update(specific_parameters)
     return (module, parameters)
