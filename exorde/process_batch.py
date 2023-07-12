fe6dfc5d1fa93087a09de4769536124bee8811c7
Mathias Dail <93382891+MathiasExorde@users.noreply.github.com>
2023-07-12 17:47:05 +0200

    Update process_batch.py

--- exorde/process_batch.py
+++ exorde/process_batch.py
@@ -1,6 +1,7 @@
 import logging
 from importlib import metadata
 from datetime import datetime
+import numpy as np
 from exorde.models import (
     ProtocolItem,
     ProtocolAnalysis,
@@ -13,14 +14,104 @@ from exorde.models import (
     Processed,
     Analysis,
 )
+from exorde.models import (
+    Translation,
+    LanguageScore,
+    Sentiment,
+    Embedding,
+    SourceType,
+    TextType,
+    Emotion,
+    Irony,
+    Age,
+    Gender,
+    Analysis,
+)
 
 from exorde.tag import tag
+from collections import Counter
 
+def Most_Common(lst):
+    data = Counter(lst)
+    return data.most_common(1)[0][0]
 
 def merge_chunks(chunks: list[ProcessedItem]) -> ProcessedItem:
-    # MERGE RULE HERE
-    return chunks[0]
+    logging.info("Merging chunks")
+    try:
+        # MERGE RULE HERE
+            categories_list = []
+            top_keywords_list = []
+            gender_list = []
+            sentiment_list = []
+            source_type_list = []
+            text_type_list = []
+            emotion_list = []
+            language_score_list = []
+            irony_list = []
+            age_list = []
+            embedding_list = []
+            for processed_item in chunks:
+                item_analysis_ = processed_item.analysis
+                categories_list.append(item_analysis_.classification)
+                top_keywords_list.append(item_analysis_.top_keywords)
+                gender_list.append(item_analysis_.gender)
+                sentiment_list.append(item_analysis_.sentiment)
+                source_type_list.append(item_analysis_.source_type)
+                text_type_list.append(item_analysis_.text_type)
+                emotion_list.append(item_analysis_.emotion)
+                language_score_list.append(item_analysis_.language_score)
+                irony_list.append(item_analysis_.irony)
+                age_list.append(item_analysis_.age)
+                embedding_list.append(item_analysis_.embedding)
 
+            logging.info("DEBUG merge_chunks")
+            ## AGGREGATED VALUES
+            ## -> classification: take the majority
+            classification_label_list = [x.label for x in categories_list]
+            category_agg = Most_Common(classification_label_list)
+            ## -> top_keywords: concatenate lists
+            keywords_agg = list()
+            for top_keywords in top_keywords_list:
+                keywords_agg.extend(top_keywords)
+            keywords_agg = Keywords(keywords_agg)
+            ## -> gender: average tuple
+            gender_male_list = [x.male for x in gender_list]
+            gender_female_list = [x.female for x in gender_list]
+            gender_agg = ()
+            for top_keywords in top_keywords_list:
+                keywords_agg.extend(top_keywords)
+            ## -> sentiment: average all sentiments, or take the MAX/MIN and make the average
+            ## -> source_type: average
+            ## -> text_type: average
+            ## -> emotion: average
+            ## -> language_score: average
+            ## -> irony: average
+            ## -> age: average
+            ## -> embedding: average
+            mean_embedding = np.mean(annotation, axis=0)
+            ####   --- REBUILD MERGED ITEM
+            merged_item = ProcessedItem(
+                    item= chunks[0].item,
+                    analysis=ProtocolAnalysis(
+                        classification=category_agg,
+                        top_keywords=processed.top_keywords,
+                        # language_score=analysis.language_score,
+                        # gender=analysis.gender,
+                        # sentiment=analysis.sentiment,
+                        # embedding=analysis.embedding,
+                        # source_type=analysis.source_type,
+                        # text_type=analysis.text_type,
+                        # emotion=analysis.emotion,
+                        # irony=analysis.irony,
+                        # age=analysis.age,
+                    ),
+                    collection_client_version=chunks[0].collection_client_version,
+                    collection_module=chunks[0].collection_module,
+                    collected_at=chunks[0].collected_at,
+                )
+    except Exception as e:
+        logging.info(f"[Merging items chunks] error: {e}")
+    return merged_item
 
 async def process_batch(
     batch: list[tuple[int, Processed]], static_configuration
