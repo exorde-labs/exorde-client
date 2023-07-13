dba4d1c7e3b0809217d579eebdae9b887da77e9a
Mathias Dail <93382891+MathiasExorde@users.noreply.github.com>
2023-07-13 17:53:54 +0200

    Update prepare_batch.py

--- exorde/prepare_batch.py
+++ exorde/prepare_batch.py
@@ -18,6 +18,7 @@ nlp_sentencer.add_pipe(sentx_pipe, before="parser")
 def evaluate_token_count(item_content_string: str, encoding_name: str = "r50k_base") -> int:
     """Returns the number of tokens in a text string."""
     try:
+        logging.info(f"HALLOOOOOOOOOO")
         if item_content_string is None or len(item_content_string)<=1:
             logging.info(f"[evaluate_token_count] the content is empty")
         encoding = tiktoken.get_encoding(encoding_name)
