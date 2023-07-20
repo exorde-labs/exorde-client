58cd7efdf888d50039436ed0f4ef53b45fc87a58
Mathias Dail <93382891+MathiasExorde@users.noreply.github.com>
2023-07-20 17:53:06 +0200

    Update lab_initialization.py

--- exorde/lab_initialization.py
+++ exorde/lab_initialization.py
@@ -41,5 +41,5 @@ def lab_initialization():
         "max_depth": 2,
         "remove_stopwords": False,
         "installed_languages": installed_languages,
-        "max_token_count": 512,
+        "max_token_count": 500,
     }
