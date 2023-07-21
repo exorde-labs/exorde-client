b16228f304ee9c3508853c8aa7b1969e7caed144
Mathias Dail <93382891+MathiasExorde@users.noreply.github.com>
2023-07-21 10:15:21 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -28,7 +28,6 @@ setup(
         "packaging",
         "numpy==1.23.4",
         "tiktoken==0.4.0",
-        "spikex==0.5.2",
         "feedparser==6.0.8",
         "python_dateutil==2.8.2",
         "newspaper3k==0.2.8",
