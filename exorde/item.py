38dc6a0e6194a9c1eb49d9b5e8e30662a9faebb1
Mathias Dail <93382891+MathiasExorde@users.noreply.github.com>
2023-09-21 17:07:07 +0200

    Update item.py

--- exorde/item.py
+++ exorde/item.py
@@ -13,32 +13,32 @@ async def get_item(
     module: ModuleType
     parameters: dict
     error_count: dict[ModuleType, int] = {}
-    try:
-        while True:
+    while True:
+        try:
             try:
-                try:
-                    module, parameters, domain = await think(
-                        command_line_arguments, counter
-                    )
-                except Exception as error:
-                    logging.exception(f"An error occured in the brain function")
-                    raise error
-                try:
-                    async for item in module.query(parameters):
-                        if isinstance(item, Item):
-                            await counter.increment(domain)
-                            yield item
-                        else:
-                            continue
-                except Exception as e:
-                    logging.exception(
-                        f"An error occured retrieving an item: %s using {module}",
-                        e,
-                    )
-                    if not error_count[module]:
-                        error_count[module] = 0
-                    error_count[module] += 1
-            except:
-                logging.exception("An error occured getting an item")
-    except GeneratorExit:
-        print("[Main loop] Generator exited, continuing...")
+                module, parameters, domain = await think(
+                    command_line_arguments, counter
+                )
+            except Exception as error:
+                logging.exception(f"An error occured in the brain function")
+                raise error
+            try:
+                async for item in module.query(parameters):
+                    if isinstance(item, Item):
+                        await counter.increment(domain)
+                        yield item
+                    else:
+                        continue                        
+            except GeneratorExit:
+                print("[Main loop] Generator exited, continuing...")
+                pass
+            except Exception as e:
+                logging.exception(
+                    f"An error occured retrieving an item: %s using {module}",
+                    e,
+                )
+                if not error_count[module]:
+                    error_count[module] = 0
+                error_count[module] += 1
+        except:
+            logging.exception("An error occured getting an item")
