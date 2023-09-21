c8f6f4ef6fe3503affa3ed933d257dbd6fa1c29f
Mathias Dail <93382891+MathiasExorde@users.noreply.github.com>
2023-09-21 16:17:42 +0200

    Update prepare_batch.py

--- exorde/prepare_batch.py
+++ exorde/prepare_batch.py
@@ -12,24 +12,10 @@ from typing import AsyncGenerator
 import tiktoken
 from ftlangdetect import detect as lang_detect
 from exorde.counter import AsyncItemCounter
-
+from exorde.evaluate_token_count import evaluate_token_count
 
 wtp = WtP("wtp-canine-s-1l")
 
-
-def evaluate_token_count(
-    item_content_string: str, encoding_name: str = "r50k_base"
-) -> int:
-    """Returns the number of tokens in a text string."""
-    try:
-        if item_content_string is None or len(item_content_string) <= 1:
-            logging.info(f"[evaluate_token_count] the content is empty")
-        encoding = tiktoken.get_encoding(encoding_name)
-        num_tokens = len(encoding.encode(item_content_string))
-    except Exception as e:
-        logging.info(f"[evaluate_token_count] error: {e}")
-        num_tokens = 0
-    return num_tokens
 
 
 def split_in_sentences(string: str):
