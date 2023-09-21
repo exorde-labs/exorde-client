d0293eb6de0b191f5465cb0860be30bad3e0e5a1
Mathias Dail <93382891+MathiasExorde@users.noreply.github.com>
2023-09-21 15:11:36 +0200

    Update process_batch.py

--- exorde/process_batch.py
+++ exorde/process_batch.py
@@ -27,8 +27,9 @@ from exorde.models import (
     Irony,
     Age,
     Gender,
-    Analysis,
+    Analysis
 )
+from exorde_data import Url
 
 from exorde.tag import tag
 from collections import Counter
