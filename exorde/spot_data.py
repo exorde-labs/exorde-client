7911e8a4f46c0b36a355cb904794cc70455f133d
Patrick Borowy <patrick.borowy@proton.me>
2023-10-05 14:28:15 +0200

    faucet if ValueError with unsuficcient founds during transaction

--- exorde/spot_data.py
+++ exorde/spot_data.py
@@ -1,6 +1,7 @@
 import logging
 from exorde.estimate_gas import estimate_gas
 import asyncio
+from exorde.faucet import faucet
 
 
 class SpottingError(Exception):
@@ -18,7 +19,7 @@ async def spot_data(
     write_web3,
 ):
     for i in range(0, 5):
-        try:            
+        try:
             logging.info(f"[Spot Data] transaction attempt ({i}/5)")
             previous_nonce = await read_web3.eth.get_transaction_count(
                 worker_account.address
@@ -51,8 +52,25 @@ async def spot_data(
             )
             logging.info(f"[Spot Data] transaction sent")
             return transaction_hash, previous_nonce
+
+        except ValueError as ve:
+            if ve.args[0].get("code", None) == -32004:
+                # Account balance is too low
+                for i in range(0, 3):
+                    try:
+                        await faucet(static_configuration)
+                        break
+                    except:
+                        timeout = i * 1.5 + 1
+                        logging.exception(
+                            f"An error occured during faucet (attempt {i}) (retry in {timeout})"
+                        )
+                        await asyncio.sleep(timeout)
+
         except Exception as e:
-            # save error
             await asyncio.sleep(i * 1.5 + 1)
-            logging.exception(f"[Spot Data] An error occured during spot_data ({i}/5)")
+            logging.exception(
+                f"[Spot Data] An error occured during spot_data ({i}/5)"
+            )
+
     raise SpottingError()
