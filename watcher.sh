c16064af7e3c90c47c6b71cc11fbd8231c25cd12
Patrick Borowy <patrick.borowy@proton.me>
2023-10-09 15:17:21 +0200

    ac01 handy script

---
+++ watcher.sh
@@ -0,0 +1,1 @@
+ac01 --certfile "/etc/letsencrypt/live/node.exorde.dev/fullchain.pem" --keyfile "/etc/letsencrypt/live/node.exorde.dev/privkey.pem" --sensitive=1 --auth-password="cannot" --scripts-folder="utilities"
