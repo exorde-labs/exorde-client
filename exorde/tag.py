8a8def4866600f6322bff21affd18a899aab68f8
Mathias Dail <93382891+MathiasExorde@users.noreply.github.com>
2023-10-16 21:01:30 +0200

    Update tag.py

--- exorde/tag.py
+++ exorde/tag.py
@@ -245,20 +245,20 @@ def tag(documents: list[str], lab_configuration):
         fin_distil_score = fdb_sentiment(text)
         fin_compounded_score = round((0.70 * fin_distil_score + 0.30 * fin_vader_sent),2)
         return fin_compounded_score
-    
+        
     def compounded_sentiment(text):
         # compounded_total_score: gen_distilbert_sentiment * 60% + vader_sentiment * 20% + compounded_fin_sentiment * 20%
         gen_distilbert_sentiment = gdb_sentiment(text)
         vader_sent = vader_sentiment(text)
         compounded_fin_sentiment = compounded_financial_sentiment(text)
         if abs(compounded_fin_sentiment) >= 0.6:
-            compounded_total_score = round((0.35 * gen_distilbert_sentiment + 0.05 * vader_sent + 0.60 * compounded_fin_sentiment),2)
+            compounded_total_score = round((0.30 * gen_distilbert_sentiment + 0.10 * vader_sent + 0.60 * compounded_fin_sentiment),2)
         elif abs(compounded_fin_sentiment) >= 0.4:
-            compounded_total_score = round((0.45 * gen_distilbert_sentiment + 0.15 * vader_sent + 0.40 * compounded_fin_sentiment),2)
+            compounded_total_score = round((0.40 * gen_distilbert_sentiment + 0.20 * vader_sent + 0.40 * compounded_fin_sentiment),2)
         elif abs(compounded_fin_sentiment) >= 0.1:
-            compounded_total_score = round((0.60 * gen_distilbert_sentiment + 0.15 * vader_sent + 0.25 * compounded_fin_sentiment),2)
+            compounded_total_score = round((0.60 * gen_distilbert_sentiment + 0.25 * vader_sent + 0.15 * compounded_fin_sentiment),2)
         else:  # if abs(compounded_fin_sentiment) < 0.1, so no apparent financial component
-            compounded_total_score = round((0.65 * gen_distilbert_sentiment + 0.35 * vader_sent),2)
+            compounded_total_score = round((0.60 * gen_distilbert_sentiment + 0.40 * vader_sent),2)
         return compounded_total_score
 
     # sentiment swifter apply compounded_sentiment
