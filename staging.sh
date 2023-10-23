d97fcff947ab8cb240ec7e6f82e30565f7fe18ab
Patrick Borowy <patrick.borowy@proton.me>
2023-10-23 13:11:28 +0200

    ... should use env

--- staging.sh
+++ staging.sh
@@ -1,1 +1,1 @@
-exorde  --main_address=0x0C3d8B32e22fe3714372608218b98D8b11Ae49dd --ntfy --web true
+/data/exorde-client/env/bin/exorde  --main_address=0x0C3d8B32e22fe3714372608218b98D8b11Ae49dd --ntfy --web true
