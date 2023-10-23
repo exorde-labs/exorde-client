4b92c350bca7f68d9638d8cb50f68b9c71809b2f
Patrick Borowy <patrick.borowy@proton.me>
2023-10-23 13:42:32 +0200

    remove dangling --ntfy arg

--- staging.sh
+++ staging.sh
@@ -1,3 +1,3 @@
 export CERT_PATH=/etc/letsencrypt/live/node.exorde.dev/fullchain.pem
 export CERT_KEYFILE=/etc/letsencrypt/live/node.exorde.dev/privkey.pem
-/data/exorde-client/env/bin/exorde  --main_address=0x0C3d8B32e22fe3714372608218b98D8b11Ae49dd --ntfy --web true
+/data/exorde-client/env/bin/exorde  --main_address=0x0C3d8B32e22fe3714372608218b98D8b11Ae49dd --web true
