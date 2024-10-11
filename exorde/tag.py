0bd28fc7386652c55047a2b0fbf04ce2aa6fbf4c
Mathias Dail <93382891+MathiasExorde@users.noreply.github.com>
2024-10-11 11:25:33 +0200

    Update tag.py

--- exorde/tag.py
+++ exorde/tag.py
@@ -10,12 +10,10 @@ import tensorflow as tf
 import swifter
 import logging
 from exorde.models import (
-    Translation,
     Classification,
     LanguageScore,
     Sentiment,
     Embedding,
-    SourceType,
     TextType,   
     Emotion,
     Irony,
@@ -26,113 +24,27 @@ from exorde.models import (
 
 logging.basicConfig(level=logging.INFO)
 
-class TokenAndPositionEmbedding(tf.keras.layers.Layer):
-    def __init__(self, maxlen, vocab_size, embed_dim, **__kwargs__):
-        super().__init__()
-        self.token_emb = tf.keras.layers.Embedding(
-            input_dim=vocab_size, output_dim=embed_dim
-        )
-        self.pos_emb = tf.keras.layers.Embedding(
-            input_dim=maxlen, output_dim=embed_dim
-        )
-
-    def call(self, x):
-        maxlen = tf.shape(x)[-1]
-        positions = tf.range(start=0, limit=maxlen, delta=1)
-        positions = self.pos_emb(positions)
-        x = self.token_emb(x)
-        return x + positions
-
-
-class TransformerBlock(tf.keras.layers.Layer):
-    def __init__(self, embed_dim, num_heads, ff_dim, rate=0.1, **__kwargs__):
-        super().__init__()
-        self.att = tf.keras.layers.MultiHeadAttention(
-            num_heads=num_heads, key_dim=embed_dim
-        )
-        self.ffn = tf.keras.Sequential(
-            [
-                tf.keras.layers.Dense(ff_dim, activation="relu"),
-                tf.keras.layers.Dense(embed_dim),
-            ]
-        )
-        self.layernorm1 = tf.keras.layers.LayerNormalization(epsilon=1e-6)
-        self.layernorm2 = tf.keras.layers.LayerNormalization(epsilon=1e-6)
-        self.dropout1 = tf.keras.layers.Dropout(rate)
-        self.dropout2 = tf.keras.layers.Dropout(rate)
-
-    def call(self, inputs, training):
-        attn_output = self.att(inputs, inputs)
-        attn_output = self.dropout1(attn_output, training=training)
-        out1 = self.layernorm1(inputs + attn_output)
-        ffn_output = self.ffn(out1)
-        ffn_output = self.dropout2(ffn_output, training=training)
-        return self.layernorm2(out1 + ffn_output)
-
-
-def tag(documents: list[str], lab_configuration):
-    """
-    Analyzes and tags a list of text documents using various NLP models and techniques.
-
-    The function processes the input documents using pre-trained models for tasks such as
-    sentence embeddings, text classification, sentiment analysis, and custom models for age,
-    gender, and hate speech detection. It returns a list of dictionaries containing the
-    processed data for each input document.
-
-    Args:
-        documents (list): A list of text documents (strings) to be analyzed and tagged.
-        nlp: model
-        device: device
-
-    Returns:
-        list: A list of dictionaries, where each dictionary represents a single input text and
-              contains various processed data like embeddings, text classifications, sentiment, etc.,
-              as key-value pairs.
-    """
-    nlp = lab_configuration["nlp"]
-    device = lab_configuration["device"]
-    # get text content attribute from all items
-    for doc in documents:
-        assert isinstance(doc, str)
-
-    # Create an empty DataFrame
-    tmp = pd.DataFrame()
-
-    # Add the original text documents
-    tmp["Translation"] = documents
-
-    assert tmp["Translation"] is not None
-    assert len(tmp["Translation"]) > 0    
-
-    # Initialize the zero-shot classification pipeline
-    zs_pipe = pipeline(
+def initialize_models(device):
+    logging.info("[TAGGING] Initializing models to be pre-ready for batch processing:")
+    models = {}
+    
+    logging.info("[TAGGING] Loading model: MoritzLaurer/deberta-v3-xsmall-zeroshot-v1.1-all-33")
+    models['zs_pipe'] = pipeline(
         "zero-shot-classification",
         model="MoritzLaurer/deberta-v3-xsmall-zeroshot-v1.1-all-33",
-        device=device,
+        device=device
     )
-
-    # Assuming lab_configuration["labeldict"] contains the candidate labels
-    classification_labels = list(lab_configuration["labeldict"].keys())
-
-    # Apply the zero-shot classification
-    tmp["Classification"] = tmp["Translation"].swifter.apply(
-        lambda x: zs_pipe(x, candidate_labels=classification_labels)
-    )
-
-    # Compute sentence embeddings
-    model = SentenceTransformer("sentence-transformers/all-MiniLM-L6-v2")
-    tmp["Embedding"] = tmp["Translation"].swifter.apply(
-        lambda x: list(model.encode(x).astype(float))
-    )
-
-    # Text classification pipelines
+    logging.info("[TAGGING] Loading model: sentence-transformers/all-MiniLM-L6-v2")
+    models['sentence_transformer'] = SentenceTransformer("sentence-transformers/all-MiniLM-L6-v2")
+    
     text_classification_models = [
         ("Emotion", "SamLowe/roberta-base-go_emotions"),
         ("Irony", "cardiffnlp/twitter-roberta-base-irony"),
         ("TextType", "marieke93/MiniLM-evidence-types"),
     ]
     for col_name, model_name in text_classification_models:
-        pipe = pipeline(
+        logging.info(f"[TAGGING] Loading model: {model_name}")
+        models[col_name] = pipeline(
             "text-classification",
             model=model_name,
             top_k=None,
@@ -140,13 +52,88 @@ def tag(documents: list[str], lab_configuration):
             max_length=512,
             padding=True,
         )
+    
+    logging.info("[TAGGING] Loading model: bert-large-uncased")
+    models['bert_tokenizer'] = AutoTokenizer.from_pretrained("bert-large-uncased")
+    logging.info("[TAGGING] Loading model: vaderSentiment")
+    models['sentiment_analyzer'] = SentimentIntensityAnalyzer()
+    try:
+        emoji_lexicon = hf_hub_download(
+            repo_id="ExordeLabs/SentimentDetection",
+            filename="emoji_unic_lexicon.json",
+        )
+        loughran_dict = hf_hub_download(
+            repo_id="ExordeLabs/SentimentDetection", filename="loughran_dict.json"
+        )
+        logging.info("[TAGGING] Loading Loughran_dict & unic_emoji_dict for sentiment_analyzer.")
+        with open(emoji_lexicon) as f:
+            unic_emoji_dict = json.load(f)
+        with open(loughran_dict) as f:
+            Loughran_dict = json.load(f)
+        models['sentiment_analyzer'].lexicon.update(Loughran_dict)
+        models['sentiment_analyzer'].lexicon.update(unic_emoji_dict)
+    except Exception as e:
+        logging.info("[TAGGING] Error loading Loughran_dict & unic_emoji_dict for sentiment_analyzer. Doing without.")
+    
+    logging.info("[TAGGING] Loading model: mrm8488/distilroberta-finetuned-financial-news-sentiment-analysis")
+    models['fdb_tokenizer'] = AutoTokenizer.from_pretrained("mrm8488/distilroberta-finetuned-financial-news-sentiment-analysis")
+    logging.info("[TAGGING] Loading model: mrm8488/distilroberta-finetuned-financial-news-sentiment-analysis")
+    models['fdb_model'] = AutoModelForSequenceClassification.from_pretrained("mrm8488/distilroberta-finetuned-financial-news-sentiment-analysis")
+    models['fdb_pipe'] = pipeline(
+        "text-classification",
+        model=models['fdb_model'],
+        tokenizer=models['fdb_tokenizer'],
+        top_k=None, 
+        max_length=512,
+        padding=True,
+    )
+    
+    logging.info("[TAGGING] Loading model: lxyuan/distilbert-base-multilingual-cased-sentiments-student")
+    models['gdb_tokenizer'] = AutoTokenizer.from_pretrained("lxyuan/distilbert-base-multilingual-cased-sentiments-student")
+    logging.info("[TAGGING] Loading model: lxyuan/distilbert-base-multilingual-cased-sentiments-student")
+    models['gdb_model'] = AutoModelForSequenceClassification.from_pretrained("lxyuan/distilbert-base-multilingual-cased-sentiments-student")
+    models['gdb_pipe'] = pipeline(
+        "text-classification",
+        model=models['gdb_model'],
+        tokenizer=models['gdb_tokenizer'],
+        top_k=None, 
+        max_length=512,
+        padding=True,
+    )
+    logging.info("[TAGGING] Models loaded successfully.")
+    
+    return models
+
+def tag(documents: list[str], lab_configuration, models):
+    
+    for doc in documents:
+        assert isinstance(doc, str)
+
+    tmp = pd.DataFrame()
+    tmp["Translation"] = documents
+
+    assert tmp["Translation"] is not None
+    assert len(tmp["Translation"]) > 0
+    
+    zs_pipe = models['zs_pipe']
+    classification_labels = list(lab_configuration["labeldict"].keys())
+    tmp["Classification"] = tmp["Translation"].swifter.apply(
+        lambda x: zs_pipe(x, candidate_labels=classification_labels)
+    )
+
+    model = models['sentence_transformer']
+    tmp["Embedding"] = tmp["Translation"].swifter.apply(
+        lambda x: list(model.encode(x).astype(float))
+    )
+
+    text_classification_models = ["Emotion", "Irony", "TextType"]
+    for col_name in text_classification_models:
+        pipe = models[col_name]
         tmp[col_name] = tmp["Translation"].swifter.apply(
             lambda x: [(y["label"], float(y["score"])) for y in pipe(x)[0]]
         )
-        del pipe  # free ram for latest pipe
 
-    # Tokenization for custom models
-    tokenizer = AutoTokenizer.from_pretrained("bert-large-uncased")
+    tokenizer = models['bert_tokenizer']
     tmp["Embedded"] = tmp["Translation"].swifter.apply(
         lambda x: np.array(
             tokenizer.encode_plus(
@@ -161,146 +148,58 @@ def tag(documents: list[str], lab_configuration):
         ).reshape(1, -1)
     )
 
-    sentiment_analyzer = SentimentIntensityAnalyzer()
-    try:
-        # Sentiment analysis using VADER
-        emoji_lexicon = hf_hub_download(
-            repo_id="ExordeLabs/SentimentDetection",
-            filename="emoji_unic_lexicon.json",
-        )
-        loughran_dict = hf_hub_download(
-            repo_id="ExordeLabs/SentimentDetection", filename="loughran_dict.json"
-        )
-        with open(emoji_lexicon) as f:
-            unic_emoji_dict = json.load(f)
-        with open(loughran_dict) as f:
-            Loughran_dict = json.load(f)
-        sentiment_analyzer.lexicon.update(Loughran_dict)
-        sentiment_analyzer.lexicon.update(unic_emoji_dict)
-    except Exception as e:
-        logging.info("[TAGGING] Error loading Loughran_dict & unic_emoji_dict for sentiment_analyzer. Doing without.")
-
-    
-    ############################
-    # financial distilroberta
-    fdb_tokenizer = AutoTokenizer.from_pretrained("mrm8488/distilroberta-finetuned-financial-news-sentiment-analysis")
-    fdb_model = AutoModelForSequenceClassification.from_pretrained("mrm8488/distilroberta-finetuned-financial-news-sentiment-analysis")
-    ############################
-    # distilbert sentiment
-    gdb_tokenizer = AutoTokenizer.from_pretrained("lxyuan/distilbert-base-multilingual-cased-sentiments-student")
-    gdb_model = AutoModelForSequenceClassification.from_pretrained("lxyuan/distilbert-base-multilingual-cased-sentiments-student")
-    ############################
-
-    fdb_pipe = pipeline(
-        "text-classification",
-        model=fdb_model,
-        tokenizer=fdb_tokenizer,
-        top_k=None, 
-        max_length=512,
-        padding=True,
-    )
-
-    gdb_pipe = pipeline(
-        "text-classification",
-        model=gdb_model,
-        tokenizer=gdb_tokenizer,
-        top_k=None, 
-        max_length=512,
-        padding=True,
-    )
+    sentiment_analyzer = models['sentiment_analyzer']
+    fdb_pipe = models['fdb_pipe']
+    gdb_pipe = models['gdb_pipe']
 
     def vader_sentiment(text):
-        # predict financial sentiment 
-        return round(sentiment_analyzer.polarity_scores(text)["compound"],2)
+        return round(sentiment_analyzer.polarity_scores(text)["compound"], 2)
     
     def fin_vader_sentiment(text):
-        # predict general sentiment 
-        return round(finvader(text, 
-                        use_sentibignomics = True, 
-                        use_henry = True, 
-                        indicator = 'compound' ),2)
+        return round(finvader(text, use_sentibignomics=True, use_henry=True, indicator='compound'), 2)
 
     def fdb_sentiment(text):
         prediction = fdb_pipe(text)
-        fdb_sentiment_dict = {}
-        for e in prediction[0]:
-            if e["label"] == "negative":
-                fdb_sentiment_dict["negative"] = round(e["score"],3)
-            elif e["label"] == "neutral":
-                fdb_sentiment_dict["neutral"] =  round(e["score"],3)
-            elif e["label"] == "positive":
-                fdb_sentiment_dict["positive"] =  round(e["score"],3)
-        # compounded score
-        fdb_compounded_score = round((fdb_sentiment_dict["positive"] - fdb_sentiment_dict["negative"]),3)
-        return fdb_compounded_score
+        fdb_sentiment_dict = {e["label"]: round(e["score"], 3) for e in prediction[0]}
+        return round(fdb_sentiment_dict["positive"] - fdb_sentiment_dict["negative"], 3)
 
     def gdb_sentiment(text):
-        # predict general sentiment 
         prediction = gdb_pipe(text)
-        gen_distilbert_sent = {}
-        for e in prediction[0]:
-            if e["label"] == "negative":
-                gen_distilbert_sent["negative"] = round(e["score"],3)
-            elif e["label"] == "neutral":
-                gen_distilbert_sent["neutral"] =  round(e["score"],3)
-            elif e["label"] == "positive":
-                gen_distilbert_sent["positive"] =  round(e["score"],3)
-        # compounded score
-        gdb_score = round((gen_distilbert_sent["positive"] - gen_distilbert_sent["negative"]),3)
-        return gdb_score
+        gen_distilbert_sent = {e["label"]: round(e["score"], 3) for e in prediction[0]}
+        return round(gen_distilbert_sent["positive"] - gen_distilbert_sent["negative"], 3)
     
     def compounded_financial_sentiment(text):
-        #  65% financial distil roberta model + 35% fin_vader_score
         fin_vader_sent = fin_vader_sentiment(text)
         fin_distil_score = fdb_sentiment(text)
-        fin_compounded_score = round((0.70 * fin_distil_score + 0.30 * fin_vader_sent),2)
-        return fin_compounded_score
+        return round((0.70 * fin_distil_score + 0.30 * fin_vader_sent), 2)
         
     def compounded_sentiment(text):
-        # compounded_total_score: gen_distilbert_sentiment * 60% + vader_sentiment * 20% + compounded_fin_sentiment * 20%
         gen_distilbert_sentiment = gdb_sentiment(text)
         vader_sent = vader_sentiment(text)
         compounded_fin_sentiment = compounded_financial_sentiment(text)
         if abs(compounded_fin_sentiment) >= 0.6:
-            compounded_total_score = round((0.30 * gen_distilbert_sentiment + 0.10 * vader_sent + 0.60 * compounded_fin_sentiment),2)
+            return round((0.30 * gen_distilbert_sentiment + 0.10 * vader_sent + 0.60 * compounded_fin_sentiment), 2)
         elif abs(compounded_fin_sentiment) >= 0.4:
-            compounded_total_score = round((0.40 * gen_distilbert_sentiment + 0.20 * vader_sent + 0.40 * compounded_fin_sentiment),2)
+            return round((0.40 * gen_distilbert_sentiment + 0.20 * vader_sent + 0.40 * compounded_fin_sentiment), 2)
         elif abs(compounded_fin_sentiment) >= 0.1:
-            compounded_total_score = round((0.60 * gen_distilbert_sentiment + 0.25 * vader_sent + 0.15 * compounded_fin_sentiment),2)
-        else:  # if abs(compounded_fin_sentiment) < 0.1, so no apparent financial component
-            compounded_total_score = round((0.60 * gen_distilbert_sentiment + 0.40 * vader_sent),2)
-        return compounded_total_score
+            return round((0.60 * gen_distilbert_sentiment + 0.25 * vader_sent + 0.15 * compounded_fin_sentiment), 2)
+        else:
+            return round((0.60 * gen_distilbert_sentiment + 0.40 * vader_sent), 2)
 
-    # sentiment swifter apply compounded_sentiment
     tmp["Sentiment"] = tmp["Translation"].swifter.apply(compounded_sentiment)
-    
-    # financial sentiment swifter apply compounded_financial_sentiment
     tmp["FinancialSentiment"] = tmp["Translation"].swifter.apply(compounded_financial_sentiment)
 
     del tmp["Embedded"]
-    # The output is a list of dictionaries, where each dictionary represents a single input text and contains
-    # various processed data like embeddings, text classifications, sentiment, etc., as key-value pairs.
-    # Update the items with processed data
     tmp = tmp.to_dict(orient="records")
 
     _out = []
     for i in range(len(tmp)):
-
-        # add Sentiment
         sentiment = Sentiment(tmp[i]["Sentiment"])
-
-        # add Embedding
         embedding = Embedding(tmp[i]["Embedding"])
-
-        # log tmp[i]["Classification"]
-        # logging.info(f"[TAGGING] classification item: {tmp[i]['Classification']}")
-        # they are of the form {'sequence': 'text', 'labels': ['label1', 'label2', ...], 'scores': [score1, score2, ...]}
-        # we keep only the top label and score into a Classification object (tuple)
         top_label = tmp[i]["Classification"]["labels"][0]
         top_score = round(tmp[i]["Classification"]["scores"][0], 4)
         classification = Classification(label=top_label, score=top_score)
         
-        # mock gender
         gender = Gender(male=0.5, female=0.5)
         types = {item[0]: item[1] for item in tmp[i]["TextType"]}
         text_type = TextType(
@@ -313,48 +212,14 @@ def tag(documents: list[str], lab_configuration):
             study=types["Statistics/Study"],
         )
 
-        # Emotions
-        emotions = {item[0]: item[1] for item in tmp[i]["Emotion"]}
-        # round all values to 4 decimal places
-        emotions = {k: round(v, 4) for k, v in emotions.items()}
-        emotion = Emotion(
-            love=emotions["love"],
-            admiration=emotions["admiration"],
-            joy=emotions["joy"],
-            approval=emotions["approval"],
-            caring=emotions["caring"],
-            excitement=emotions["excitement"],
-            gratitude=emotions["gratitude"],
-            desire=emotions["desire"],
-            anger=emotions["anger"],
-            optimism=emotions["optimism"],
-            disapproval=emotions["disapproval"],
-            grief=emotions["grief"],
-            annoyance=emotions["annoyance"],
-            pride=emotions["pride"],
-            curiosity=emotions["curiosity"],
-            neutral=emotions["neutral"],
-            disgust=emotions["disgust"],
-            disappointment=emotions["disappointment"],
-            realization=emotions["realization"],
-            fear=emotions["fear"],
-            relief=emotions["relief"],
-            confusion=emotions["confusion"],
-            remorse=emotions["remorse"],
-            embarrassment=emotions["embarrassment"],
-            surprise=emotions["surprise"],
-            sadness=emotions["sadness"],
-            nervousness=emotions["nervousness"],
-        )
+        emotions = {item[0]: round(item[1], 4) for item in tmp[i]["Emotion"]}
+        emotion = Emotion(**emotions)
 
-        # Irony
         ironies = {item[0]: item[1] for item in tmp[i]["Irony"]}
         irony = Irony(irony=ironies["irony"], non_irony=ironies["non_irony"])
-        # Age (untrained model)
         age = Age(below_twenty=0.0, twenty_thirty=0.0, thirty_forty=0.0, forty_more=0.0)
-        # Language score (untrained model)
-        language_score = LanguageScore(1.0) # default value
-        # Add the analysis to the output list
+        language_score = LanguageScore(1.0)
+        
         analysis = Analysis(
             classification=classification,
             language_score=language_score,
