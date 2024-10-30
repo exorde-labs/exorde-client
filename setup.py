561fde78b4873f00778d34683292e6b0cf65015d
Mathias Dail <93382891+MathiasExorde@users.noreply.github.com>
2024-10-31 00:15:44 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="exorde",
-    version="v2.5.11",
+    version="v2.5.12",
     author="Exorde Labs",
     author_email="hello@exordelabs.com",
     description="The AI-based client to mine data and power the Exorde Network",
