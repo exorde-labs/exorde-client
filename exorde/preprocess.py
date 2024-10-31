2537272e8a2c3cfd8aa9c9ac226a96f83b3b4947
Mathias Dail <93382891+MathiasExorde@users.noreply.github.com>
2024-10-31 10:15:49 +0100

    Update preprocess.py

--- exorde/preprocess.py
+++ exorde/preprocess.py
@@ -44,8 +44,6 @@ def preprocess_text(text: str, remove_stopwords: bool) -> str:
 
 
 def preprocess(item, remove_stopwords):
-    logging.info("CONTENT TEXT = %s",item)
-    logging.info("CONTENT TYPE = %s",type(item))
-    # item.content = Content(preprocess_text(item.content, remove_stopwords))
+    item.content = Content(str(item.content))
     # item.content = Content(item.content.replace("\n", " "))
     return item
